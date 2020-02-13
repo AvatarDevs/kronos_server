323c7a77fdbf093631c72668546f40dab9076082
Colby <colbymaloydev@gmail.com>
2020-02-13 01:34:54 -0600

    initial commit after create

---
+++ lib/kronos_server.dart
@@ -0,0 +1,11 @@
+/// kronos_server
+///
+/// A Aqueduct web server.
+library kronos_server;
+
+export 'dart:async';
+export 'dart:io';
+
+export 'package:aqueduct/aqueduct.dart';
+
+export 'channel.dart';
